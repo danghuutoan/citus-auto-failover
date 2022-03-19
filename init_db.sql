00431087614c9d84c0104152049f52fc9fc8dca3
danghuutoan <danghuutoan2002@gmail.com>
2022-03-19 09:34:41 +0700

    init workspace

--- init_db.sql
+++ init_db.sql
@@ -1,10 +1,3 @@
-```
-\copy companies from '/data/companies.csv' with csv
-\copy campaigns from '/data/campaigns.csv' with csv
-\copy ads from '/data/ads.csv' with csv
-```
-
-```
 CREATE TABLE companies (
     id bigint NOT NULL,
     name text NOT NULL,
@@ -37,40 +30,11 @@ CREATE TABLE ads (
     created_at timestamp without time zone NOT NULL,
     updated_at timestamp without time zone NOT NULL
 );
-```
 
-```
 ALTER TABLE companies ADD PRIMARY KEY (id);
 ALTER TABLE campaigns ADD PRIMARY KEY (id, company_id);
 ALTER TABLE ads ADD PRIMARY KEY (id, company_id);
-```
 
-```
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @sink.json
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://localhost:8083/connectors/ -d @source.json
-```
-
-```
-kafka-topics.sh --list --bootstrap-server kafka:9092
-```
-
-
-insert into entity_attribute_group (
-    entity_id, attribute_group, attribute_version, json_value
-)
-select
-    left(md5(i::text), 10),
-    left(md5(random()::text), 28),
-    i,
-    '{"key": "value"}'
-from generate_series(1, 1000000) s(i)
-
-move shards out of master node
-citus_drain_node(node_name, nodeport)
-
-excute on worker
-SELECT * from citus_add_node('worker-101', 5432);
-SELECT * from citus_add_node('worker-101', 5432);
-
-verify
-SELECT * FROM citus_get_active_worker_nodes();
+\copy companies from '/data/companies.csv' with csv
+\copy campaigns from '/data/campaigns.csv' with csv
+\copy ads from '/data/ads.csv' with csv
